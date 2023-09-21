f5d5f766f9bade5a6b4637fd6bf33f1cff36b222
Shiki <xshiki@users.noreply.github.com>
2023-09-22 00:57:13 +0200

    Backup

--- Assets/FloorTextOverlay.cs
+++ Assets/FloorTextOverlay.cs
@@ -8,8 +8,8 @@ public class FloorTextOverlay : MonoBehaviour
     public int floorCounter = 1;
     public TextMeshProUGUI gameOverScore;
     public void SetFloorText()
-    {
-        Debug.Log("set floor test triggered");
+    { 
+    
         TextOverlayManager.Instance.SetNewTextOverlay("Floor "+ floorCounter.ToString());
         gameOverScore.text = "Reached Floor: "+ floorCounter.ToString();
         floorCounter++;
